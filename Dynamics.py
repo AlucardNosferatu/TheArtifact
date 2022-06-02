da66f266106a1188a1e74b03cef4b1f8199fc727
Ebenezer Scrooge <1641367382@qq.com>
2022-06-02 22:06:22 +0800

    Update

--- Dynamics.py
+++ Dynamics.py
@@ -1,23 +1,132 @@
-def obj_update_obj1(obj, world):
-    obj['v_x'] = 10
-    obj['v_y'] -= 1
-    if obj['v_y'] < -10:
-        obj['v_y'] = -10
-    if obj['y'] < world['frameHeight'] - 300:
-        obj['v_y'] = 0
-    elif obj['y'] < world['frameHeight'] - 200:
-        obj['v_y'] = -4
-    elif obj['y'] < world['frameHeight'] - 100:
-        obj['v_y'] = -8
-    obj['pitch'] = abs(obj['v_y']) * 5
+import math
 
 
 class Artifact:
-    acc = 0.5
-    max_speed = 10
+    ThisSpecs = None
+    ThisStats = None
+    ThisInput = None
+
+    def __init__(self):
+        self.ThisInput = Input(art_inst=self)
+        self.ThisStats = Stats()
+        self.ThisSpecs = Specs()
+
+    def update_stats(self, drag, gravity):
+        self.ThisStats.update_loc()
+
+        pit = self.ThisInput.pitch_sig
+        maa = self.ThisSpecs.max_ang_acc
+        mas = self.ThisSpecs.max_ang_spd
+        self.ThisStats.update_ang(pit_sig=pit, max_ang_acc=maa, max_ang_spd=mas, drag=drag)
+
+        thr = self.ThisInput.throttle
+        ma = self.ThisSpecs.max_acc
+        ms = self.ThisSpecs.max_spd
+        self.ThisStats.update_spd(throttle=thr, max_acc=ma, max_spd=ms, drag=drag, gravity=gravity)
+
+
+class Specs:
+    max_acc = 9
+    max_spd = 12
+    max_ang_acc = 2
+    max_ang_spd = 4
+
+    def __init__(self):
+        self.max_acc = 3
+        self.max_spd = 12
+        self.max_ang_acc = 3
+        self.max_ang_spd = 5
+
+
+class Stats:
+    location = [0.0, 0.0]
+    speed = [0.0, 0.0]
+    acc = [0.0, 0.0]
+    ang = 0.0
+    ang_speed = 0.0
+
+    def __init__(self):
+        self.location = [0.0, 0.0]
+        self.speed = [0.0, 0.0]
+        self.acc = [0.0, 0.0]
+        self.ang = 0.0
+        self.ang_speed = 0.0
+
+    def update_ang(self, pit_sig, max_ang_acc, max_ang_spd, drag):
+        self.ang_speed += pit_sig * max_ang_acc
+        if self.ang_speed > 1:
+            self.ang_speed -= math.sqrt(0.5 * drag)
+        elif self.ang_speed < -1:
+            self.ang_speed += math.sqrt(0.5 * drag)
+        if self.ang_speed > max_ang_spd:
+            self.ang_speed = max_ang_spd
+        elif -self.ang_speed > max_ang_spd:
+            self.ang_speed = -max_ang_spd
+
+        self.ang += self.ang_speed
+
+    def update_spd(self, throttle, max_acc, max_spd, drag, gravity):
+        self.acc = throttle * max_acc
+        self.acc -= drag
+        if self.acc > max_acc:
+            self.acc = max_acc
+        elif self.acc < 0:
+            self.acc = 0.0
+
+        dv_x = self.acc * math.cos(self.ang * 3.14 / 180)
+        dv_y = self.acc * -math.sin(self.ang * 3.14 / 180)
+        print(self.acc, math.cos(self.ang * 3.14 / 180), math.sin(self.ang * 3.14 / 180))
+        if self.location[1] > 0:
+            dv_y -= gravity
+        self.speed[0] += dv_x
+        self.speed[1] += dv_y
+        spd_mag = math.sqrt(self.speed[0] ** 2 + self.speed[1] ** 2)
+        if spd_mag > max_spd:
+            ratio = max_spd / spd_mag
+            self.speed[0] *= ratio
+            self.speed[1] *= ratio
+
+    def update_loc(self):
+        self.location[0] += self.speed[0]
+        self.location[1] += self.speed[1]
+        print('speed', self.speed)
+        if self.location[1] < 0:
+            self.location[1] = 0
+        if self.location[0] < 0:
+            self.location[0] = 0
+        elif self.location[0] > 1023:
+            self.location[0] = 1023
+
+
+class Input:
+    throttle = 0
+    pitch_sig = 0
+    artifact_instance = None
+
+    def __init__(self, art_inst: Artifact):
+        self.throttle = 0.0
+        self.pitch_sig = 0.0
+        self.artifact_instance = art_inst
 
     def pitch_up(self):
-        pass
+        self.pitch_sig += 0.01
+        if self.pitch_sig > 1:
+            self.pitch_sig = 1
 
     def pitch_down(self):
-        pass
+        self.pitch_sig -= 0.01
+        if self.pitch_sig < -1:
+            self.pitch_sig = -1
+
+    def pitch_neutral(self):
+        self.pitch_sig = 0
+
+    def throttle_up(self):
+        self.throttle += 0.01
+        if self.throttle > 1:
+            self.throttle = 1
+
+    def throttle_down(self):
+        self.throttle -= 0.01
+        if self.throttle < 0:
+            self.throttle = 0
---
+++ Trash/World_Deprecated.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy
+
+world = {'frameWidth': 1024, 'frameHeight': 768}
+obj_list = []
+
+pic = cv2.imread('../mark.png')
+r, c = pic.shape[:2]
+
+
+def obj_update_obj1(obj, world):
+    obj['v_x'] = 10
+    obj['v_y'] -= 1
+    if obj['v_y'] < -10:
+        obj['v_y'] = -10
+    if obj['y'] < world['frameHeight'] - 300:
+        obj['v_y'] = 0
+    elif obj['y'] < world['frameHeight'] - 200:
+        obj['v_y'] = -4
+    elif obj['y'] < world['frameHeight'] - 100:
+        obj['v_y'] = -8
+    obj['pitch'] = abs(obj['v_y']) * 5
+
+
+obj_1 = {
+    'x_c': 0 + round(c / 2),
+    'y_c': world['frameHeight'] - round(r / 2),
+    'pic': pic,
+    'cols': c,
+    'rows': r,
+    'v_x': 0,
+    'v_y': 0,
+    'pitch': 0,
+    'update_phy': obj_update_obj1
+}
+obj_1.__setitem__('x', obj_1['x_c'] - round(obj_1['cols'] / 2))
+obj_1.__setitem__('y', obj_1['y_c'] - round(obj_1['rows'] / 2))
+
+obj_list.append(obj_1)
+
+
+def rotate_image(image, angle):
+    image_center = tuple(numpy.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+
+def physics_frame():
+    global obj_list
+    for obj in obj_list:
+        obj['update_phy'](obj, world)
+        # dx = random.randint(-10, 20)
+        # dy = random.randint(-10, 20)
+        dx = obj['v_x']
+        dy = obj['v_y']
+        obj['x_c'] = max(round(obj['cols'] / 2), min(obj['x_c'] + dx, world['frameWidth'] - round(obj['cols'] / 2)))
+        obj['y_c'] = max(round(obj['rows'] / 2), min(obj['y_c'] + dy, world['frameHeight'] - round(obj['rows'] / 2)))
+        obj.__setitem__('x', obj['x_c'] - round(obj['cols'] / 2))
+        obj.__setitem__('y', obj['y_c'] - round(obj['rows'] / 2))
+
+
+def draw_frame():
+    global obj_list
+    canvas = numpy.ones([world['frameHeight'], world['frameWidth'], 3], dtype=numpy.uint8) * 255
+    for obj in obj_list:
+        y1 = obj['y']
+        y2 = obj['y'] + obj['rows']
+        x1 = obj['x']
+        x2 = obj['x'] + obj['cols']
+        r_pic = rotate_image(obj['pic'], obj['pitch'])
+        canvas[y1:y2, x1:x2] = r_pic
+    cv2.imshow("Title", canvas)
+    cv2.waitKey(1)
+
+
+f_count = 0
+while True:
+    if f_count >= 10:
+        f_count = 0
+        physics_frame()
+    draw_frame()
+    f_count += 1
+
+cap.release()
+cv2.destroyAllWindows()
