304801a5e36736589385c975e722b187cb071dc9
Ebenezer Scrooge <1641367382@qq.com>
2022-06-04 20:58:41 +0800

    Update Base.py

--- Base.py
+++ Base.py
@@ -1,5 +1,8 @@
+import random
 import uuid
-import json
+
+from Avionics import *
+from Designs import Engine, Warhead, Locomotive, Avionics, Weapon, Chassis, Part
 
 
 class Base:
@@ -10,6 +13,8 @@ class Base:
     hr_basic = None
     buildings = None
     time_passed_tasks = None
+    unlocked_parts = None
+    loaded_designs = None
 
     def __init__(self):
         self.tech_tree = {
@@ -39,6 +44,15 @@ class Base:
         }
         self.buildings = [None, None, None]
         self.time_passed_tasks = []
+        self.unlocked_parts = {
+            'chs': [],
+            'eng': [],
+            'wpn': [],
+            'whd': [],
+            'loc': [],
+            'avi': []
+        }
+        self.loaded_designs = {}
 
     def next_building_slot(self):
         for i in range(len(self.buildings)):
@@ -78,49 +92,253 @@ class Building:
         self.base_ptr = b_ptr
 
 
+class CommandCenter(Building):
+    def __init__(self, slot, b_ptr: Base):
+        super().__init__(2000, slot, 1, 'command_center', 'normal', b_ptr)
+
+
+class Laboratory(Building):
+    part_class_dict = {
+        'chs': Chassis,
+        'eng': Engine,
+        'wpn': Weapon,
+        'whd': Warhead,
+        'loc': Locomotive,
+        'avi': Avionics
+    }
+    basic_component = {
+        'chs': ['wood'],
+        'eng': ['steel'],
+        'wpn': ['barrel'],
+        'whd': ['gun_p', 'steel'],
+        'loc': ['wood', 'ti'],
+        'avi': ['e-device']
+    }
+    experience_modifier = 0.0
+    avi_function = [radar_scan, jammer, irst_scan]
+    parts_names = {
+        'chs': '载具骨架',
+        'eng': '引擎',
+        'wpn': '发射器',
+        'whd': '弹头',
+        'loc': '机动部件',
+        'avi': '电子设备'
+    }
+    current_work: list[None | list[Part | int]] = None
+    need_days = 0
+
+    def __init__(self, slot, b_ptr: Base):
+        super().__init__(2000, slot, 1, 'laboratory', 'normal', b_ptr)
+        self.experience_modifier = 1.0
+        self.current_work = [None, None, None]
+        self.need_days = 7
+
+    def research_new_part(self, part_type):
+        if None not in self.current_work:
+            print('实验室正忙！请等待当前工作完成！')
+            return
+
+        def gen_size(pt):
+            if pt == 'chs':
+                sz = -1
+            else:
+                sz = random.choice([0, 1, 2])
+            return sz
+
+        def gen_hp(sz, pt):
+            if sz == 0:
+                health = random.randint(10, 100)
+            elif sz == 1:
+                health = random.randint(50, 140)
+            elif sz == 2:
+                health = random.randint(90, 180)
+            else:
+                health = random.randint(130, 220)
+            if pt == 'whd':
+                health = round(health / 2)
+            return health
+
+        def gen_bc(sz, pt):
+            build_cost = {}
+            for res in Laboratory.basic_component[pt]:
+                if sz == 0:
+                    build_cost.__setitem__(res, random.randint(10, 30))
+                elif sz == 1:
+                    build_cost.__setitem__(res, random.randint(20, 40))
+                elif sz == 2:
+                    build_cost.__setitem__(res, random.randint(30, 50))
+                else:
+                    build_cost.__setitem__(res, random.randint(40, 60))
+            return build_cost
+
+        def gen_ep(sz, pt):
+            extra_params = []
+            if pt == 'chs':
+                slots_count = {}
+                for pt2 in Laboratory.part_class_dict:
+                    if pt2 not in ['chs', 'whd']:
+                        slots_count.__setitem__(
+                            pt2,
+                            [
+                                random.randint(1, 5),
+                                random.randint(1, 4),
+                                random.randint(1, 3)
+                            ]
+                        )
+                extra_params.append(slots_count)
+                extra_params.append(min(random.random() + 0.5, 1))
+            elif pt == 'wpn':
+                e_range = random.randint(50, 100)
+                acc = random.randint(50, 150 - e_range)
+                rof = random.randint(50, 200 - acc - e_range)
+                if sz == 0:
+                    # small wpn
+                    e_range = max(25, e_range - 20)
+                    acc = min(100, acc + 10)
+                    rof = min(100, rof + 10)
+                elif sz == 2:
+                    # large wpn
+                    e_range = min(125, e_range + 20)
+                    acc = max(50, acc - 10)
+                    rof = max(50, rof - 10)
+                extra_params.append(rof)
+                extra_params.append(acc)
+                extra_params.append(e_range)
+            elif pt == 'whd':
+                damage = random.randint(50, 75)
+                spd = random.randint(50, 150 - damage)
+                ang_spd = random.randint(50, 200 - spd - damage)
+                if sz == 0:
+                    # small whd
+                    damage = max(50, damage - 20)
+                    spd = min(75, spd + 10)
+                    ang_spd = min(100, ang_spd + 10)
+                elif sz == 2:
+                    # large whd
+                    damage = min(125, damage + 20)
+                    spd = max(75, spd - 10)
+                    ang_spd = max(25, ang_spd - 10)
+                dt = random.choice(['normal', 'AP', 'napalm', 'shock', 'toxic', 'corrosive', 'HE', 'radioactive'])
+                extra_params.append(damage)
+                extra_params.append(dt)
+                extra_params.append(spd)
+                extra_params.append(ang_spd)
+            elif pt == 'avi':
+                func1 = random.choice(Laboratory.avi_function)
+                func2 = random.choice([None, func1])
+                if func2 is not None:
+                    while func2 == func1:
+                        func2 = random.choice(Laboratory.avi_function)
+                extra_params.append(func1)
+                extra_params.append(func2)
+            elif pt == 'loc':
+                m = random.randint(10, 30) + 10 * sz
+                d = m + random.randint(-5, 5) + 10 * sz
+                extra_params.append(m)
+                extra_params.append(d)
+            elif pt == 'eng':
+                mt = random.randint(10, 30) + 10 * sz
+                fc = mt + random.randint(-5, 5) + 10 * sz
+                extra_params.append(mt)
+                extra_params.append(fc)
+            else:
+                print('零件种类代号错误！额外参数设为None')
+                extra_params = None
+            return extra_params
+
+        size = gen_size(part_type)
+        hp = gen_hp(size, part_type)
+        bc = gen_bc(size, part_type)
+
+        if part_type in ['chs', 'wpn', 'whd', 'avi', 'loc', 'eng']:
+            ep = gen_ep(size, part_type)
+            new_part = Laboratory.part_class_dict[part_type](hp=hp, bc=bc, size=size, extra_params=ep)
+        else:
+            new_part = Laboratory.part_class_dict[part_type](hp=hp, bc=bc, size=size)
+
+        work_slot_index = self.current_work.index(None)
+        self.current_work[work_slot_index] = [new_part, self.need_days]
+        if self not in self.base_ptr.time_passed_tasks:
+            self.base_ptr.time_passed_tasks.append(self)
+
+    def dispose_old_part(self, part_type, part_index):
+        self.base_ptr.unlocked_parts[part_type].pop(part_index)
+
+    def terminate_ongoing_res(self, work_index):
+        if 0 <= work_index < len(self.current_work):
+            if self.current_work[work_index] is None:
+                print('这个研究室没有正在进行的研究')
+            else:
+                self.current_work[work_index] = None
+                print('已中止第', work_index, '实验室的研究')
+        else:
+            print('参数错误！')
+
+    def tomorrow(self):
+        finished = []
+        unfinished: list
+        for index, unfinished in enumerate(self.current_work):
+            if unfinished is not None:
+                unfinished[1] -= 1
+                if unfinished[1] <= 0:
+                    finished.append(index)
+                    new_part: Part = unfinished[0]
+                    part_type = new_part.type_str
+                    self.base_ptr.unlocked_parts[part_type].append(new_part)
+                    print('你获得了新', Laboratory.parts_names[part_type], '！属性如下：')
+                    attrs = [item for item in dir(new_part) if not item.startswith('__')]
+                    for attr in attrs:
+                        attr_val = getattr(new_part, attr)
+                        if not hasattr(attr_val, '__call__'):
+                            print(attr, attr_val)
+        for fin in finished:
+            self.current_work[fin] = None
+
+
 class ConstructionCrane(Building):
     progress_record = {}
-    cost_dict = None
-    class_dict = None
+    cost_dict = {
+        'command_center': {'wood': 10, 'time': 2},
+        'laboratory': {'wood': 10, 'time': 2},
+
+    }
+    class_dict = {
+        'command_center': CommandCenter,
+        'laboratory': Laboratory
+    }
 
     def __init__(self, slot, base_inst):
         super().__init__(2000, slot, 1, 'construction_crane', 'normal', base_inst)
         self.progress_record = {}
-        self.cost_dict = {
-            'command_center': {'wood': 10, 'time': 2},
-            'warehouse': {'wood': 10, 'time': 0},
-
-        }
-        self.class_dict = {
-            'command_center': CommandCenter
-        }
 
     def query_cost(self, building_type):
-        print(self.cost_dict[building_type])
+        print(ConstructionCrane.cost_dict[building_type])
+        print()
+        print(self.base_ptr.warehouse_basic)
 
     def build_new(self, slot, building_type):
-        if building_type not in self.class_dict:
+        if building_type not in ConstructionCrane.class_dict:
             print('不可建造或建筑类型代码错误！')
         else:
             if 0 <= slot < len(self.base_ptr.buildings):
                 if self.base_ptr.buildings[slot] is not None:
                     print('这个位置有建筑了！')
                 else:
-                    for res in self.cost_dict[building_type]:
+                    for res in ConstructionCrane.cost_dict[building_type]:
                         if res != 'time':
-                            if self.cost_dict[building_type][res] > self.base_ptr.warehouse_basic[res]:
+                            if ConstructionCrane.cost_dict[building_type][res] > self.base_ptr.warehouse_basic[res]:
                                 print('资源不足，无法建造！')
                                 return
-                    for res in self.cost_dict[building_type]:
+                    for res in ConstructionCrane.cost_dict[building_type]:
                         if res != 'time':
-                            self.base_ptr.consume_resource(res, self.cost_dict[building_type][res])
+                            self.base_ptr.consume_resource(res, ConstructionCrane.cost_dict[building_type][res])
                     new_b_id = str(uuid.uuid4())
                     self.base_ptr.buildings[slot] = new_b_id
                     self.progress_record.__setitem__(
                         new_b_id,
                         [
                             building_type,
-                            self.cost_dict[building_type]['time']
+                            ConstructionCrane.cost_dict[building_type]['time']
                         ]
                     )
                     if self not in self.base_ptr.time_passed_tasks:
@@ -142,9 +360,9 @@ class ConstructionCrane(Building):
                         del self.progress_record[removed_building]
                     else:
                         building_type = removed_building.type_str
-                    for res in self.cost_dict[building_type]:
+                    for res in ConstructionCrane.cost_dict[building_type]:
                         if res != 'time':
-                            self.base_ptr.add_resource(res, self.cost_dict[building_type][res])
+                            self.base_ptr.add_resource(res, ConstructionCrane.cost_dict[building_type][res])
                     self.base_ptr.buildings[slot] = None
                     del removed_building
         else:
@@ -158,7 +376,7 @@ class ConstructionCrane(Building):
             if unf_building[1] <= 0:
                 building_type = unf_building[0]
                 slot = self.base_ptr.buildings.index(unfinished)
-                self.base_ptr.buildings[slot] = self.class_dict[building_type](
+                self.base_ptr.buildings[slot] = ConstructionCrane.class_dict[building_type](
                     slot=slot,
                     b_ptr=self.base_ptr
                 )
@@ -166,19 +384,6 @@ class ConstructionCrane(Building):
                 print('新的', building_type, '在空地', slot, '完工了！')
         for fin in finished:
             del self.progress_record[fin]
-
-
-class CommandCenter(Building):
-    def __init__(self, slot, b_ptr: Base):
-        super().__init__(2000, slot, 1, 'command_center', 'normal', b_ptr)
-
-
-class Laboratory(Building):
-    loaded_designs = None
-
-    def __init__(self, slot, b_ptr: Base):
-        super().__init__(2000, slot, 1, 'laboratory', 'normal', b_ptr)
-        loaded_designs={}
 
 
 if __name__ == '__main__':
