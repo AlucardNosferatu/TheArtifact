6c9f8f3542f5c62e145cf83013f5183eea92dca7
Ebenezer Scrooge <1641367382@qq.com>
2022-09-01 17:26:45 +0800

    Update Physics.py

--- Physics.py
+++ Physics.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 import pygame
 from Box2D.b2 import world, polygonShape, staticBody, dynamicBody, fixtureDef
 from pygame.locals import (QUIT, KEYDOWN, K_ESCAPE)
@@ -10,7 +12,7 @@ def body_vertices_2_pygame_polygon(shape):
 
 
 PPM = 20.0  # pixels per meter
-TARGET_FPS = 60
+TARGET_FPS = 15
 TIME_STEP = 1.0 / TARGET_FPS
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 
@@ -25,18 +27,21 @@ ground_body = world.CreateStaticBody(
     position=(16, 2),
     shapes=polygonShape(box=(15, 1))
 )
-fixture = fixtureDef(shape=polygonShape(box=(2, 1)), density=1)
-dynamic_body = world.CreateDynamicBody(position=(12, 10), angle=0, fixtures=fixture)
+fixture = fixtureDef(shape=polygonShape(box=(1, 2)), density=1)
+dynamic_body = world.CreateDynamicBody(position=(12, 11), angle=0, fixtures=fixture)
 dynamic_body2 = world.CreateDynamicBody(position=(10, 15), angle=0, fixtures=fixture)
-# joint = world.CreateRevoluteJoint(
-#     bodyA=dynamic_body,
-#     bodyB=dynamic_body2,
-#     anchor=(dynamic_body.worldCenter + dynamic_body2.worldCenter) / 2)
+joint = world.CreateRevoluteJoint(
+    bodyA=dynamic_body,
+    bodyB=dynamic_body2,
+    anchor=(dynamic_body.worldCenter + dynamic_body2.worldCenter) / 2,
+    collideConnected=True
+)
 colors = {
     staticBody: (255, 255, 255, 255),
     dynamicBody: (127, 127, 127, 255),
 }
 running = True
+joint_exist = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
@@ -47,9 +52,14 @@ while running:
         for fixture in body.fixtures:
             vertices = body_vertices_2_pygame_polygon(fixture.shape)
             pygame.draw.polygon(screen, colors[body.type], vertices)
-
+    if joint_exist:
+        x, y = joint.GetReactionForce(10)
+        f = sqrt((x ** 2) + (y ** 2))
+        print(f)
+        if f > 500:
+            world.DestroyJoint(joint)
+            joint_exist = False
     world.Step(TIME_STEP, 10, 10)
-
     pygame.display.flip()
     clock.tick(TARGET_FPS)
 
@@ -57,6 +67,6 @@ pygame.quit()
 print('Done!')
 
 # Basic Physics:√
-# Joint Collision:×
-# Breakable Connection:×
+# Joint Collision:√
+# Breakable Connection:√
 # Lift & Drag:×
