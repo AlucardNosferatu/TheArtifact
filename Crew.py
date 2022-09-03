52fd16233bc5422de5c902e3cf4556a79bd5a0fd
Ebenezer Scrooge <1641367382@qq.com>
2022-09-03 10:44:24 +0800

    Update Crew.py

--- Crew.py
+++ Crew.py
@@ -34,6 +34,8 @@ class Crew:
     dead: bool
     ship: Vessel
     current_route: list[int]
+    walk_spd: float
+    mov_proc: float
 
     def __init__(self, crew_name, ship):
         self.health = 100
@@ -43,13 +45,16 @@ class Crew:
         self.loc = [0, 0]
         self.ship = ship
         self.current_route = []
+        self.walk_spd = 10.0
+        self.mov_proc = 0.0
 
     def spawn_at(self, x: int, y: int):
         if self.ship.parts_matrix[x][y] is not None:
             self.loc[0] = x
             self.loc[1] = y
+            print(self.name, 'is at', self.loc)
 
-    def work_at(self):
+    def work_here(self):
         x = self.loc[0]
         y = self.loc[1]
         if self.ship.parts_matrix[x][y] is not None:
@@ -58,7 +63,7 @@ class Crew:
     def goto_order(self, x, y):
         self.current_route.clear()
         node_count = len(self.ship.crew_map)
-        node_cost = [[np.inf for i in range(0, 3)] for i in range(node_count)]
+        node_cost: list[list[int | float]] = [[np.inf for i in range(0, 3)] for i in range(node_count)]
         for i in range(node_count):
             node_cost[i][0] = i
         node0 = self.ship.pos2index[str(self.loc[0]) + '_' + str(self.loc[1])]
@@ -73,13 +78,24 @@ class Crew:
         target_pos = node1
         while target_pos not in close_list:
             node0 = choose_min(node_cost, close_list)  # 找node_cost最小的节点
-            [node_cost, close_list] = count_cost(self.ship.crew_map, node_cost, close_list, node0, node_count)  # 计算邻节点
+            node_cost, close_list = count_cost(self.ship.crew_map, node_cost, close_list, node0, node_count)  # 计算邻节点
         self.current_route.append(node1)
         print("最短的路径代价为:", node_cost[node1][1])
         while crew_pos not in self.current_route:
             node1 = node_cost[node1][2]
             self.current_route.append(node1)
         print("最短路径为：", self.current_route)
+
+    def what_now(self):
+        if len(self.current_route) > 0:
+            if self.mov_proc < 100.0:
+                self.mov_proc += self.walk_spd
+            else:
+                self.mov_proc = 0.0
+                r, c = [int(c_str) for c_str in self.ship.index2pos[self.current_route.pop(-1)].split('_')]
+                self.spawn_at(r, c)
+        else:
+            self.work_here()
 
 
 if __name__ == '__main__':
@@ -103,6 +119,4 @@ if __name__ == '__main__':
     crew1 = Crew('Linhaobo', v)
     crew1.spawn_at(0, 0)
     crew1.goto_order(2, 2)
-    crew1.goto_order(1, 1)
-    crew1.goto_order(1, 2)
-    crew1.goto_order(2, 1)
+    
