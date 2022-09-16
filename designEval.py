cbffe5a426df96aceba3cec35e9cea089cfb2906
Ebenezer Scrooge <1641367382@qq.com>
2022-09-16 22:17:37 +0800

    Update designEval.py

--- designEval.py
+++ designEval.py
@@ -10,52 +10,45 @@ import cv2
 import matplotlib.pyplot as plt
 from WindTunnel.lbm import pylbm
 
-# %%
-pad = {'left': 75, 'right': 75, 'top': 50, 'bottom': 5}
 
-L_car = 7 * 25.4
-fn_img = r'WindTunnel/content/car.png'
-fn_out = 'car_out.png'
-fn_out2 = 'car_diagram.png'
-nu = 1 / 6
-
-# load car image
-a = cv2.cvtColor(cv2.imread(fn_img), cv2.COLOR_BGR2GRAY)
-rescale = False
-
-if rescale:
-    scale = .5
-    # rescale if necessary
-    if scale != 1:
-        t_dim = (int(a.shape[1] * scale), int(a.shape[0] * scale))
-        a = cv2.resize(a, t_dim, interpolation=cv2.INTER_AREA)
-    pixel_size = L_car / a.shape[1]
-else:
-    # fixed car resolution
-    LB_car = 300
-    f = LB_car / a.shape[1]
-    t_dim = (int(a.shape[1] * f), int(a.shape[0] * f))
-    a = cv2.resize(a, t_dim, interpolation=cv2.INTER_AREA)
-    pixel_size = L_car / a.shape[1]
-
-# make binary
-a = (a < 127).astype(int)
-# calc pixel size
-
-# pad shape
-M = np.pad(a, [(int(pad['top'] / pixel_size), int(pad['bottom'] / pixel_size)),
-               (int(pad['left'] / pixel_size), int(pad['right'] / pixel_size))], 'constant', constant_values=0)
-
-plt.imshow(M)
-
-# fluid points just left of the car
-kL = np.where(np.roll(M, (0, -1), axis=(0, 1)) > M)
-kR = np.where(np.roll(M, (0, 1), axis=(0, 1)) > M)
-kU = np.where(np.roll(M, (-1, 0), axis=(0, 1)) > M)
-kB = np.where(np.roll(M, (1, 0), axis=(0, 1)) > M)
+def load_img(fn_img=r'WindTunnel/content/car.png'):
+    return cv2.cvtColor(cv2.imread(fn_img), cv2.COLOR_BGR2GRAY)
 
 
-def drag(self):
+def preprocess(array, rescale=False):
+    L_car = 7 * 25.4
+    if rescale:
+        scale = .5
+        # rescale if necessary
+        if scale != 1:
+            t_dim = (int(array.shape[1] * scale), int(array.shape[0] * scale))
+            array = cv2.resize(array, t_dim, interpolation=cv2.INTER_AREA)
+        p_size = L_car / array.shape[1]
+    else:
+        # fixed car resolution
+        LB_car = 300
+        f = LB_car / array.shape[1]
+        t_dim = (int(array.shape[1] * f), int(array.shape[0] * f))
+        array = cv2.resize(array, t_dim, interpolation=cv2.INTER_AREA)
+        p_size = L_car / array.shape[1]
+    array = (array < 127).astype(int)
+    return array, p_size
+
+
+def pad_shape(array, p_size):
+    pad = {'left': 75, 'right': 75, 'top': 50, 'bottom': 5}
+    padded = np.pad(array, [(int(pad['top'] / p_size), int(pad['bottom'] / p_size)),
+                            (int(pad['left'] / p_size), int(pad['right'] / p_size))], 'constant', constant_values=0)
+    return padded
+
+
+def drag(self, padded):
+    nu = 1 / 6
+    # fluid points just left of the car
+    kL = np.where(np.roll(padded, (0, -1), axis=(0, 1)) > padded)
+    kR = np.where(np.roll(padded, (0, 1), axis=(0, 1)) > padded)
+    kU = np.where(np.roll(padded, (-1, 0), axis=(0, 1)) > padded)
+    kB = np.where(np.roll(padded, (1, 0), axis=(0, 1)) > padded)
     # normal forces
     P = self.fields['rho'][0, :, :, 0] / 3
     fxN = sum(P[kL]) - sum(P[kR])
@@ -89,15 +82,8 @@ def drag(self):
     return P
 
 
-# %%
-S = pylbm.LBM((1, *M.shape))
-S.fields['ns'][0, :, :, 0] = M  # car
-# S.fields['ns'][0,-1,:,:]=1  # road
-
-mx, my = np.meshgrid(range(M.shape[1]), range(M.shape[0]))
-
-
-def my_plot(self):
+def my_plot(self, padded):
+    mx, my = np.meshgrid(range(padded.shape[1]), range(padded.shape[0]))
     # velocity
     v = self.fields['v'][0]
     vx = v[..., 2]
@@ -105,7 +91,7 @@ def my_plot(self):
     v_mag = ((v ** 2).sum(axis=-1)) ** 0.5
 
     # -- for display
-    v_mag[np.where(M == 1)] = v_mag.max()
+    v_mag[np.where(padded == 1)] = v_mag.max()
 
     # velocity difference
     dv = (((self.fields['v'] - self.V_old) ** 2).sum(axis=-1)) ** 0.5
@@ -118,11 +104,11 @@ def my_plot(self):
         sys.exit()
 
     # calc drag
-    P = drag(self)
+    P = drag(self, padded)
     fx = self.hist['fx'][-1]
     fy = self.hist['fy'][-1]
     # -- for display
-    P[np.where(M == 1)] = P.min()
+    P[np.where(padded == 1)] = P.min()
 
     # display
     plt.figure(figsize=(12, 6))
@@ -144,7 +130,7 @@ def my_plot(self):
     plt.streamplot(mx, my, vx, vy, color='r', density=.8)
     plt.tight_layout()
 
-    plt.savefig(fn_out, dpi=200)
+    # plt.savefig(fn_out, dpi=200)
     plt.show()
 
     # diagnostic
@@ -177,7 +163,7 @@ def my_plot(self):
     plt.legend()
 
     plt.tight_layout()
-    plt.savefig(fn_out2, dpi=200)
+    # plt.savefig(fn_out2, dpi=200)
     plt.show()
 
 
@@ -191,20 +177,27 @@ def cb_vel(self):
 
     self.fields['v'][0, :, -1, :] = self.fields['v'][0, :, -2, :]  # open-right
     self.fields['v'][0, 0, :, :] = self.fields['v'][0, 1, :, :]  # open-top
-
+    padded = self.padded
     if self.step % 10 == 0:
         dv = (((self.fields['v'] - self.V_old) ** 2).sum(axis=-1)) ** 0.5
         max_dv = dv.max()
         # noinspection PyUnusedLocal
-        P = drag(self)
+        P = drag(self, padded)
         # self.hist['step'].append(self.step)
         # self.hist['fx'].append(fx)
         # self.hist['fy'].append(fy)
         self.hist['dv_max'].append(max_dv)
 
     if (self.step > 0) and (self.step % 100 == 0):
-        my_plot(self)
+        my_plot(self, padded)
 
+
+a = load_img()
+a, pixel_size = preprocess(a)
+M = pad_shape(a, pixel_size)
+S = pylbm.LBM((1, *M.shape))
+S.padded = M
+S.fields['ns'][0, :, :, 0] = M  # car
 
 # track how the velocity profile changes
 S.V_old = S.fields['v'].copy()
