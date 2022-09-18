5dbcc31cc3bb807c7d1365601919c31e70100f9c
Ebenezer Scrooge <1641367382@qq.com>
2022-09-18 21:51:06 +0800

    Update RadarSim.py

--- RadarSim.py
+++ RadarSim.py
@@ -7,7 +7,6 @@ Created on Sun Nov 25 16:15:36 2018
 @author: jaymz
 """
 
-import copy
 import math
 
 import numpy as np
@@ -15,7 +14,6 @@ import pygame
 
 from AeroSim import conv_vert
 from RayTracer import opticalElement
-from RayTracer.menu import Menu
 from RayTracer.opticalElement import FlatMirror, CurvedMirror, OpticalElement
 
 
@@ -46,7 +44,7 @@ class CurvedReceiver(OpticalElement):
         rx, ry, vx, vy, R = ray_pos[0] - center[0], ray_pos[1] - center[1], ray_dir[0], ray_dir[1], self.boundaries[1]
         angExtent = np.arctan(self.boundaries[0] / R) * 180 / np.pi
         if (2 * rx * vx + 2 * ry * vy) ** 2 - 4 * (-R ** 2 + rx ** 2 + ry ** 2) * (vx ** 2 + vy ** 2) < 0:
-            self.properties['color'] = RED
+            # self.properties['color'] = RED
             return None
         t_val1 = (-2 * rx * vx - 2 * ry * vy - np.sqrt(
             (2 * rx * vx + 2 * ry * vy) ** 2 - 4 * (-R ** 2 + rx ** 2 + ry ** 2) * (vx ** 2 + vy ** 2))) / (
@@ -88,7 +86,7 @@ class CurvedReceiver(OpticalElement):
             self.properties['color'] = GREEN
             return None
         else:
-            self.properties['color'] = RED
+            # self.properties['color'] = RED
             return None
 
     def reflect(self, ray_pos, ray_dir, intersect):
@@ -162,10 +160,7 @@ def direct_draw_arc(element):
     c_y = screen_y + dy_from_c
     top_left_x = c_x - radius
     top_left_y = c_y - radius
-    color = BLUE
-    if isinstance(element.properties, dict):
-        color = element.properties['color']
-
+    color = element.properties['color']
     b1 = (element.orientation - (ang_ext / 2)) * np.pi / 180
     b2 = (element.orientation + (ang_ext / 2)) * np.pi / 180
     rect_ellipse = [
@@ -184,287 +179,43 @@ def direct_draw_arc(element):
     )
 
 
-def screen_map_function(pos):
-    range_x = abs(coord_lims[0][0] - coord_lims[0][1])
-    range_y = abs(coord_lims[1][0] - coord_lims[1][1])
-    scale_x = float(size[0]) / range_x
-    scale_y = float(size[1]) / range_y
-    return (
-        int((pos[0] - coord_lims[0][0]) / range_x * size[0]),
-        int((-pos[1] - coord_lims[1][0]) / range_y * size[1]),
-        scale_x,
-        scale_y)
-
-
-def screen_map_inv(pos_screen):
-    range_x = abs(coord_lims[0][0] - coord_lims[0][1])
-    range_y = abs(coord_lims[1][0] - coord_lims[1][1])
-    scale_x = float(size[0]) / range_x
-    scale_y = float(size[1]) / range_y
-    return (
-        int((pos_screen[0] / size[0] * range_x + coord_lims[0][0])),
-        int(-pos_screen[1] * range_y / size[1] - coord_lims[1][0]),
-        scale_x, scale_y)
-
-
-def add_flat_mirror():
-    x_, y_, _, _ = screen_map_inv(mousePos)
-    elements.append(opticalElement.FlatMirror([x_, y_], 45, 100, {'color': RED}))
-
-
-def add_curved_mirror():
-    x_, y_, _, _ = screen_map_inv(mousePos)
-    elements.append(opticalElement.CurvedMirror([x_, y_], 45, [100, 1000], {'color': BLACK}))
-
-
-# -------- Ray Tracing ---------------
-
-# noinspection PyPep8Naming
 def ray_trace():
     output_rays = [rays]
-    first_round = True
     for i in range(0, MAX_BOUNCE):
-        newRays = []
-        # print("i = %d" % i)
+        new_rays = []
         for r in output_rays[-1]:
             r: dict[str, None]
-            minDist = np.inf
-            closestElem = None
-            closestIntersect = None
+            min_dist = np.inf
+            closest_elem = None
+            closest_intersect = None
             for elem in elements:
-                if elem.elementType() == 'CurvedReceiver' and first_round:
-                    continue
+                if elem.elementType() == 'CurvedReceiver':
+                    if r['color'] == GREEN:
+                        continue
                 intersect = elem.rayIntersection(r['pos'], r['dir'])
+                if elem.orientation == 105:
+                    print('105', intersect, r)
                 if intersect is None:
                     continue
                 dist = np.linalg.norm(intersect - r['pos'])
                 if dist < .0001:
                     continue
-                #                if(closestElem is not None and elem == closestElem):
-                #                    continue
-                if minDist > dist:
-                    minDist = dist
-                    closestElem = elem
-                    closestIntersect = intersect
-            if closestElem is not None:
-                dir_new = closestElem.reflect(r['pos'], r['dir'], closestIntersect)
+                if min_dist > dist:
+                    min_dist = dist
+                    closest_elem = elem
+                    closest_intersect = intersect
+            if closest_elem is not None:
+                dir_new = closest_elem.reflect(r['pos'], r['dir'], closest_intersect)
                 dir_new = dir_new / np.linalg.norm(dir_new)
-                r['intersect'] = closestIntersect
-                newRays.append({'pos': closestIntersect, 'dir': dir_new, 'color': BLUE})
+                r['intersect'] = closest_intersect
+                new_rays.append({'pos': closest_intersect, 'dir': dir_new, 'color': BLUE})
             else:
                 r['intersect'] = None
-        if len(newRays) != 0:
-            output_rays.append(newRays)
-            first_round = False
-
+        if len(new_rays) != 0:
+            output_rays.append(new_rays)
+        else:
+            break
     return output_rays
-
-
-def ray_sim():
-    global mousePos, coord_lims
-    pygame.init()
-
-    # Used to manage how fast the screen updates
-    clock = pygame.time.Clock()
-
-    zoom_factor = 1.3
-    pygame.display.set_caption("Ray Tracer")
-
-    # Loop until the user clicks the close button.
-    end = False
-
-    theta = 0
-    rays.append(
-        {'pos': np.array([-1000, 710]), 'dir': np.array([np.cos(np.pi * theta / 180), np.sin(np.pi * theta / 180)]),
-         'color': RED})
-    theta = 0.2
-    rays.append(
-        {'pos': np.array([-1000, 710]), 'dir': np.array([np.cos(np.pi * theta / 180), np.sin(np.pi * theta / 180)]),
-         'color': GREEN})
-    theta = -0.2
-    rays.append(
-        {'pos': np.array([-1000, 710]), 'dir': np.array([np.cos(np.pi * theta / 180), np.sin(np.pi * theta / 180)]),
-         'color': BLUE})
-
-    # ------------Right Click Menu Functions-----------------------
-
-    menu_entries = ['Flat Mirror', 'Curved Mirror']
-    right_click_menu = Menu([0, 0], menu_entries, rightClick=True, activated=False)
-    right_click_menu.assignFunction(0, add_flat_mirror)
-    right_click_menu.assignFunction(1, add_curved_mirror)
-    # -------- Main program loop---------------
-
-    # Keep track of mouse manipulations
-    mouse_selection_element_index: int | None = None
-    mouse_near_element_index = None
-
-    rotating_element_index = None
-    scaling_element_index = None
-
-    view_drag_mouse_start = None
-    view_drag_c_start = None
-
-    while not end:
-        for event in pygame.event.get():  # Handle events
-            right_click_menu.processMouseInput(event)
-            if event.type == pygame.QUIT:
-                print("User asked to quit.")
-            # --------Keyboard Events---------------
-            elif event.type == pygame.KEYDOWN:
-                print(event)
-                x, y, _, _ = screen_map_inv(mousePos)
-                if event.unicode == 'n':
-                    elements.append(opticalElement.FlatMirror(np.array([x, y]), -45, 100, {'color': BLACK}))
-                elif event.unicode == '\x08':  # Backspace
-                    if len(elements) > 0:
-                        del elements[-1]
-                elif event.unicode == 'r':  # Rotate
-                    rotating_element_index = mouse_near_element_index
-                elif event.unicode == 's':  # Scale
-                    scaling_element_index = mouse_near_element_index
-                elif event.unicode == '0':  # Reset View
-                    coord_lims = coord_lims_default
-                elif event.unicode == 'd':
-                    if mouse_near_element_index is not None:
-                        del elements[mouse_near_element_index]
-                        mouse_near_element_index = None
-                elif event.key == 282:
-                    coord_lims = np.array(coord_lims) * 2
-                elif event.key == 283:
-                    coord_lims = np.array(coord_lims) / 2
-                elif event.key == 276:  # Left Arrow
-                    coord_lims = np.array(coord_lims) - (coord_lims[0][1] - coord_lims[0][0]) / 10 * np.array(
-                        [[1.0, 1.0], [0.0, 0.0]])
-                elif event.key == 275:  # Right Arrow
-                    coord_lims = np.array(coord_lims) + (coord_lims[0][1] - coord_lims[0][0]) / 10 * np.array(
-                        [[1.0, 1.0], [0.0, 0.0]])
-                elif event.key == 273:  # Up Arrow
-                    coord_lims = np.array(coord_lims) - (coord_lims[1][1] - coord_lims[1][0]) / 10 * np.array(
-                        [[0.0, 0.0], [1.0, 1.0]])
-                elif event.key == 274:  # Down Arrow
-                    coord_lims = np.array(coord_lims) + (coord_lims[1][1] - coord_lims[1][0]) / 10 * np.array(
-                        [[0.0, 0.0], [1.0, 1.0]])
-            elif event.type == pygame.KEYUP:
-                print("User let go of a key.")
-
-            # -------Mouse Events--------
-            elif event.type == pygame.MOUSEBUTTONDOWN:  # Mouse click events
-                print(event)
-                mousePos = np.array(pygame.mouse.get_pos())
-                if event.button == 1:
-                    for i in range(0, len(elements)):
-                        if elements[i].checkIfMouseNear(mousePos, screen_map_function):
-                            print("Grabbed element %d" % i)
-                            mouse_selection_element_index = i
-                    if mouse_selection_element_index is None:
-                        x, y, scale_x, scale_y = screen_map_inv(mousePos)
-                        view_drag_mouse_start = np.array([x, y])
-                        view_drag_c_start = copy.deepcopy(coord_lims)
-                elif event.button == 3:
-                    continue
-
-                elif event.button == 4:
-                    x, y, scale_x, scale_y = screen_map_inv(mousePos)
-                    coord_lims[0][0] = (coord_lims[0][0] - x) * zoom_factor + x
-                    coord_lims[0][1] = (coord_lims[0][1] - x) * zoom_factor + x
-                    coord_lims[1][0] = (coord_lims[1][0] + y) * zoom_factor - y
-                    coord_lims[1][1] = (coord_lims[1][1] + y) * zoom_factor - y
-                elif event.button == 5:
-                    x, y, scale_x, scale_y = screen_map_inv(mousePos)
-                    coord_lims[0][0] = (coord_lims[0][0] - x) / zoom_factor + x
-                    coord_lims[0][1] = (coord_lims[0][1] - x) / zoom_factor + x
-                    coord_lims[1][0] = (coord_lims[1][0] + y) / zoom_factor - y
-                    coord_lims[1][1] = (coord_lims[1][1] + y) / zoom_factor - y
-                rotating_element_index = None
-                scaling_element_index = None
-
-            elif event.type == pygame.MOUSEMOTION:  # Mouse movement events
-                mousePos = np.array(pygame.mouse.get_pos())
-                # print(mousePos)
-                # print(mouse_selection_element_index)
-                x, y, scale_x, scale_y = screen_map_inv(mousePos)
-                if mouse_selection_element_index is not None:
-                    mouse_selection_element_index: int
-                    new_pos = np.array([x, y])
-                    print("Dragging element %d to pos " % mouse_selection_element_index + str(new_pos))
-                    elements[mouse_selection_element_index].pos = new_pos
-                elif rotating_element_index is not None:
-                    rel_vect = np.array([x, y]) - elements[rotating_element_index].pos
-                    new_angle = np.arctan2(rel_vect[1], rel_vect[0])
-                    elements[rotating_element_index].setOrientation(180 * new_angle / np.pi)
-                elif scaling_element_index is not None:
-                    rel_vect = np.array([x, y]) - elements[scaling_element_index].pos
-                    new_scale = np.linalg.norm([rel_vect[1], rel_vect[0]])
-                    if elements[scaling_element_index].elementType() == 'FlatMirror':
-                        elements[scaling_element_index].boundaries = new_scale
-                    elif elements[scaling_element_index].elementType() == 'CurvedMirror':
-                        elements[scaling_element_index].boundaries[0] = new_scale
-
-                elif view_drag_mouse_start is not None:
-                    drag_vect = np.array([x, y]) - view_drag_mouse_start
-                    coord_lims = view_drag_c_start + np.array([[-drag_vect[0]] * 2, [drag_vect[1]] * 2]) / 1.2
-
-                else:
-                    mouse_near_element_index = None
-                    for i in range(0, len(elements)):
-                        mousePos = np.array(pygame.mouse.get_pos())
-                        if elements[i].checkIfMouseNear(mousePos, screen_map_function):
-                            mouse_near_element_index = i
-
-            elif event.type == pygame.MOUSEBUTTONUP:  # Mouse release event
-                mousePos = np.array(pygame.mouse.get_pos())
-                print(mousePos)
-                print(mouse_selection_element_index)
-                if mouse_selection_element_index is not None:
-                    mouse_selection_element_index: int
-                    x, y, _, _ = screen_map_inv(mousePos)
-                    print("Moved element %d from " % mouse_selection_element_index + str(
-                        elements[mouse_selection_element_index].pos) + " to pos " + str(mousePos))
-                    elements[mouse_selection_element_index].pos = np.array([x, y])
-                mouse_selection_element_index = None
-                view_drag_mouse_start = None
-
-                # Handle window close
-            if event.type == pygame.QUIT:  # If user clicked close
-                end = True  # Flag that we are done so we exit this loop
-
-        output_rays = ray_trace()
-
-        # ----------------Drawing code should go here------------
-
-        # Clear screen
-        screen.fill(WHITE)
-
-        # Draw the optical elements
-        for i in range(0, len(elements)):
-            if mouse_near_element_index is not None and mouse_near_element_index == i:
-                elements[i].drawSelected(screen, screen_map_function)
-            else:
-                elements[i].draw(screen, screen_map_function)
-
-        # Draw the rays
-        for i in range(0, len(output_rays)):
-            for j in range(0, len(output_rays[i])):
-                x1, y1, _, _ = screen_map_function(output_rays[i][j]['pos'])
-                if 'intersect' in output_rays[i][j] and output_rays[i][j]['intersect'] is not None:
-                    x2, y2, _, _ = screen_map_function(output_rays[i][j]['intersect'])
-                else:
-                    x2, y2, _, _ = screen_map_function(
-                        output_rays[i][j]['pos'] + output_rays[i][j]['dir'] * (np.max(coord_lims) - np.min(coord_lims)))
-                if 'color' in output_rays[i][j]:
-                    pygame.draw.line(screen, output_rays[i][j]['color'], [x1, y1], [x2, y2], 2)
-                else:
-                    pygame.draw.line(screen, RED, [x1, y1], [x2, y2], 2)
-
-        right_click_menu.draw(screen)
-        # Go ahead and update the screen with what we've drawn.
-        pygame.display.flip()
-
-        # Limit to 60 frames per second
-        clock.tick(60)
-
-    # Exit thread after loop has been exited
-    pygame.quit()
 
 
 def get_ray_start_p(rect_size):
@@ -529,7 +280,7 @@ def polygon_mirror(vertices):
 
 def circle_receiver(start_points, start_ang, radius):
     for i in range(len(start_points)):
-        elements.append(CurvedReceiver(start_points[i], start_ang[i], [4.9, radius], {'color': RED}))
+        elements.append(CurvedReceiver(start_points[i], start_ang[i], [5, radius], {'color': RED}))
 
 
 def radar_cross_section():
